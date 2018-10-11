8b97c81015880e7c0b2c3f3a89be31bef73b3441
Daily120 <forzhacker@mail.ru>
2018-10-12 00:17:01 +0200

    Deploying on heroku

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = ({ userName, entries }) => {
+    return (
+        <div>
+            <div className='white f3'>
+                {`${userName}, your current entry count is ...`}
+            </div>
+            <div className='white f1'>
+                {entries}
+            </div>
+        </div>
+    )
+}
+
+export default Rank;
